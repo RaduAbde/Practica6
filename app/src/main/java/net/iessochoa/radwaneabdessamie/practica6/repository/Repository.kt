1a4ea3545ccb4a88ad544970f22ca18898242524
RaduAbde <raduabde@gmail.com>
2024-01-31 19:59:00 +0100

    Repository

---
+++ app/src/main/java/net/iessochoa/radwaneabdessamie/practica6/repository/Repository.kt
@@ -0,0 +1,17 @@
+package net.iessochoa.radwaneabdessamie.practica6.repository
+
+import android.app.Application
+import android.content.Context
+import net.iessochoa.radwaneabdessamie.practica6.network.NetworkService
+
+object Repository {
+    //necesitaremos el context en las capas inferiores
+    private lateinit var application: Application
+    //iniciamos la instancia
+    operator fun invoke(context: Context) {
+        this.application = context.applicationContext as Application
+        NetworkService(application)
+    }
+    suspend fun getNextPersonajes()=NetworkService.getNextPersonajes()
+    fun getLiveDataListaPersonajes ()=NetworkService.getLiveDataListaPersonajes()
+}
